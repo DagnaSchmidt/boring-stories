7ebe9a6f518bc3bb1363b67b63e0afbee1243e04
Dagna Schmidt <dagdagi889@gmail.com>
2023-10-11 11:05:26 +0200

    menu open/close style

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -4,9 +4,15 @@ import { useSelector } from 'react-redux';
 const Menu = () => {
     const menu = useSelector(state => state.menu);
     console.log(menu);
-    
+
   return (
-    <div>Menu</div>
+    <div className={!menu.open ?
+        'bg-primary w-2 h-2 border-4 border-secondary absolute lg:right-20 md:right-8 right-4 top-[3.35rem]'
+        :
+        'bg-primary w-[19rem] max-md:w-full h-full border-4 border-secondary absolute lg:right-20 md:right-8 right-0 top-0 z-10'
+        }>
+        
+    </div>
   )
 }
 
--- frontend/src/components/ToggleMenuButton.js
+++ frontend/src/components/ToggleMenuButton.js
@@ -9,7 +9,7 @@ const ToggleMenuButton = () => {
     const dispatch = useDispatch();
 
   return (
-    <button className='pz-4 absolute lg:right-24 md:right-11 right-8 top-10 z-50' onClick={() =>dispatch(toggleMenu())}>
+    <button className='pz-4 absolute lg:right-24 md:right-12 right-8 top-10 z-50' onClick={() =>dispatch(toggleMenu())}>
         <p className='synonym text-xl font-medium tracking-wider'>
             {menu.open ? 'close' : menu.page === 'home' ? 'start here' : 'menu'}
         </p>
