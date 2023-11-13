357df2b2ee7575fbb809ff75c6eb20493e87820d
Dagna Schmidt <dagdagi889@gmail.com>
2023-11-13 09:50:41 +0100

    grid layout responsive changes

--- frontend/src/components/story/GridLayout.js
+++ frontend/src/components/story/GridLayout.js
@@ -9,7 +9,7 @@ const GridLayout = () => {
     }
 
   return (
-    <div className='absolute right-96 w-[19rem] max-sm:z-10 z-50 bg-secondary grid grid-cols-1 grid-rows-3 justify-items-center items-center h-full py-12'>
+    <div className='absolute right-96 max-lg:right-80 max-md:right-0 max-md:top-24 w-[19rem] max-md:w-full max-md:z-50 z-40 bg-secondary grid grid-cols-1 grid-rows-3 justify-items-center items-center h-full max-md:h-[calc(100%-6rem)] py-12 max-md:py-2'>
         <div className='bg-primary w-52 h-44'>x</div>
         <div className='bg-primary w-52 h-44'>x</div>
         <div className='bg-primary w-52 h-44'>x</div>
