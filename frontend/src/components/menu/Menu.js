4ffb15eeec5bb3f24b79ba681923c0055cd6729d
Dagna Schmidt <dagdagi889@gmail.com>
2023-11-13 09:08:14 +0100

    menu items style change

--- frontend/src/components/menu/Menu.js
+++ frontend/src/components/menu/Menu.js
@@ -27,28 +27,28 @@ const Menu = () => {
         {
             menu.open &&
             <ul className='flex flex-col justify-center align-middle gap-20'>
-                <li className={menu.selected === 'all' && 'opacity-30 hover:opacity-70'}>
+                <li className={menu.selected === 'new' ? 'opacity-100' : 'opacity-50 hover:opacity-100'}>
                     <button className='w-full' onClick={() => dispatch(switchSelected('new'))} disabled={menu.selected === 'new' && true}>
                         <p className='amulya text-center text-xl font-medium'>new stories</p>
                     </button>
                 </li>
-                <li className={menu.selected === 'new' && 'opacity-30 hover:opacity-70'}>
+                <li className={menu.selected === 'all' ? 'opacity-100' : 'opacity-50 hover:opacity-100'}>
                     <button className='w-full' onClick={() => dispatch(switchSelected('all'))} disabled={menu.selected === 'all' && true}>
-                        <p className='amulya text-center text-xl font-medium'>all stories</p>
+                        <p className='amulya text-center text-xl font-medium' >all stories</p>
                     </button>
                     {
                         menu.selected === 'all' &&
                             <SubMenu />
                     }
                 </li>
-                <li className={menu.selected !== 'none' && 'opacity-30 hover:opacity-70'}>
+                <li className='opacity-50 hover:opacity-100'>
                     <button className='w-full' onClick={handleDisplayRandomStory}>
                         <p className='amulya text-center text-xl font-medium'>random story</p>
                     </button>
                 </li>
             </ul>
         }
-        
+
     </div>
   )
 }
