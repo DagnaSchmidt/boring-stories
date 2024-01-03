d145d45798474adbd0506d66593241cf59c56cf6
Dagna Schmidt <dagdagi889@gmail.com>
2024-01-03 10:29:50 +0100

    images display on story component added

--- frontend/src/components/story/Story.js
+++ frontend/src/components/story/Story.js
@@ -8,7 +8,7 @@ const Story = () => {
     const dispatch = useDispatch();
 
     const story = useSelector(state => state.story);
-    const { createdAt, title, description } = story.details;
+    const { createdAt, title, description, images } = story.details;
 
     const closeStory = () => {
         dispatch(closeActiveStory());
@@ -18,20 +18,30 @@ const Story = () => {
     }
 
   return (
-        <div className='w-full'>
+        <div className='w-full h-full '>
             <button onClick={closeStory}>
                 <h2 className='text-body pt-[22px] pb-[50px] hover:cursor-pointer font-medium tracking-tighter amulya'>
                     Boring Stories
                 </h2>
             </button>
 
-            <div className='border-4 border-secondary w-full flex flex-col gap-8 px-4 py-8'>
-                <div className='flex justify-between '>
-                    <p className='amulya text-base font-medium'>{title}</p>
-                    <p className='amulya text-sm'>{createdAt}</p>
+            <div className='overflow-y-scroll h-full pb-12'>
+                <div className='border-4 border-secondary w-full flex flex-col gap-8 px-4 py-8'>
+                    <div className='flex justify-between '>
+                        <p className='amulya text-base font-medium'>{title}</p>
+                        <p className='amulya text-sm'>{createdAt}</p>
+                    </div>
+                    <p className='amulya text-sm px-4'>{description}</p>
                 </div>
-                <p className='amulya text-sm px-4'>{description}</p>
+
+                <div className='flex flex-col gap-4 py-24'>
+                    {
+                        images.map((i, index) => <img key={index} src={`https://drive.google.com/uc?export=view&id=${i}`} alt={i} />)
+                    }
+                </div>
             </div>
+
+
         </div>
   )
 }
