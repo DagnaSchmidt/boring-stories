cbf6821f557ac31f081c00e575e77691c8b65abb
Dagna Schmidt <dagdagi889@gmail.com>
2024-01-09 12:41:37 +0100

    added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './styles/index.css';
+
+//pages
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+    </Routes>
   );
 }
 
