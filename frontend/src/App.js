088579475f53c2cc5307eb9d5595c8cf056f4759
Dagna Schmidt <dagdagi889@gmail.com>
2024-01-09 14:15:31 +0100

    admin login page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import './styles/index.css';
 
 //pages
 import HomePage from './pages/HomePage';
+import AdminLogin from './components/login/AdminLogin';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
+      <Route path='/admin' element={<AdminLogin />} />
     </Routes>
   );
 }
---
+++ frontend/src/components/login/AdminLogin.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AdminLogin = () => {
+  return (
+    <div className='bg-primary w-full h-screen flex justify-center items-center'>
+      <div className='flex flex-col gap-4 items-center'>
+        <input />
+        <button>
+          <p className='synonym text-xl font-medium tracking-wider'>login</p>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default AdminLogin;
