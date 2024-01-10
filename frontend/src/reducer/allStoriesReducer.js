ce2f0c00792e369e8a3efde394b6efe9190ceadb
Dagna Schmidt <dagdagi889@gmail.com>
2024-01-10 11:05:42 +0100

    all stories reducer actions added

--- frontend/src/reducer/allStoriesReducer.js
+++ frontend/src/reducer/allStoriesReducer.js
@@ -10,31 +10,12 @@ const allStoriesSlice = createSlice({
         addReducerStory(state, action) {
             return [...state, action.payload];
         },
-        editReducerStoryDescription(state, action) {
+        editReducerStory(state, action) {
             const newState = state.map(i => {
-                if(i.id !== action.payload){
+                if(i.id !== action.payload.id){
                     return i;
                 }else{
-                    return {
-                        ...i,
-                        description: action.payload
-                    };
-                }
-            });
-            return newState;
-        },
-        editReducerStoryImages(state, action) {
-            const newState = state.map(i => {
-                if(i.id !== action.payload){
-                    return i;
-                }else{
-                    return {
-                        ...i,
-                        images: [
-                            ...i.images,
-                            ...action.payload
-                        ]
-                    };
+                    return action.payload;
                 }
             });
             return newState;
@@ -46,5 +27,51 @@ const allStoriesSlice = createSlice({
     }
 });
 
-export const { setAllReducerStories, addReducerStory, editReducerStoryDescription, editReducerStoryImages, deleteReducerStory } = allStoriesSlice.actions;
+
+export const setAllStories = () => {
+    //get all stories
+    const allStories = [];
+
+    return dispatch => {
+        dispatch(setAllReducerStories(allStories));
+    }
+}
+
+export const addStory = (storyContent) => {
+    //post new story
+    const newStory = {};
+
+    return dispatch => {
+        dispatch(addReducerStory(newStory));
+    }
+}
+
+export const editStoryDescription = (storyDescription) => {
+    //put story
+    const updatedStory = {};
+
+    return dispatch => {
+        dispatch(editReducerStory(updatedStory));
+    }
+}
+
+export const addStoryImages = (storyImages) => {
+    //put story
+    const updatedStory = {};
+
+    return dispatch => {
+        dispatch(editReducerStory(updatedStory));
+    }
+}
+
+export const deleteStory = (id) => {
+    //delete story
+    const deletedStory = {};
+
+    return dispatch => {
+        dispatch(deleteReducerStory(id));
+    }
+}
+
+export const { setAllReducerStories, addReducerStory, editReducerStory, deleteReducerStory } = allStoriesSlice.actions;
 export default allStoriesSlice.reducer;
