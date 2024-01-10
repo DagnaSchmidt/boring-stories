bc7def2f88798d4c3a693598dcc707b0edf64700
Dagna Schmidt <dagdagi889@gmail.com>
2024-01-10 11:09:24 +0100

    store updated

--- frontend/src/reducer/store.js
+++ frontend/src/reducer/store.js
@@ -2,10 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 //import reducers
 import menuReducer from "./menuReducer";
 import storyReducer from "./storyReducer";
+import allStoriesReducer from "./allStoriesReducer";
+import activeStoryReducer from "./activeStoryReducer";
 
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
+    allStories: allStoriesReducer,
+    activeStory: activeStoryReducer,
     story: storyReducer
   },
 });
