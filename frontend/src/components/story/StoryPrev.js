9f63c90869453c9ee30857098f14378ab06e10b9
Dagna Schmidt <dagdagi889@gmail.com>
2024-01-18 11:09:43 +0100

    images fix

--- frontend/src/components/story/StoryPrev.js
+++ frontend/src/components/story/StoryPrev.js
@@ -7,7 +7,6 @@ import { closeMenu } from '../../reducer/menuReducer';
 
 const StoryPrev = ({data}) => {
     const { createdAt, title, prevImg, altText, id } = data;
-    // const imageLink = `https://drive.google.com/uc?id=${prevImg}`;
     const dispatch = useDispatch();
 
     const openStory = () => {
@@ -18,7 +17,7 @@ const StoryPrev = ({data}) => {
   return (
     <div className='w-52 border-4 border-secondary bg-primary flex flex-col gap-2'>
         <button onClick={openStory} className='w-full'>
-            <img src={`https://lh3.googleusercontent.com/d/${prevImg}`} alt={altText} className='grayscale hover:grayscale-0 w-full h-32 object-cover' />
+            <img src={`https://lh3.google.com/u/0/d/${prevImg}`} alt={altText} className='grayscale hover:grayscale-0 w-full h-32 object-cover' />
         </button>
         <div className='flex justify-between items-end px-1 pb-1'>
             <p className='amulya text-base font-medium'>{title}</p>
